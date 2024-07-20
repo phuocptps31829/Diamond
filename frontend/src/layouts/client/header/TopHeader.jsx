450ba351885dfc4260c1c6e2f04b53f38600df40
sw-n2-9h <chinhnguyennn24@gmail.com>
2024-07-20 22:24:24 +0700

    new ui folder

--- frontend/src/layouts/client/header/TopHeader.jsx
+++ frontend/src/layouts/client/header/TopHeader.jsx
@@ -1,7 +1,7 @@
 export default function TopHeader() {
     return (
         <div className="bg-primary-500">
-            <div className="flex items-center mx-auto text-white max-w-screen-2xl px-3 py-1 sm:px-10 justify-between">
+            <div className="flex mx-auto items-center text-white max-w-screen-2xl px-3 py-1 sm:px-10 justify-between">
                 <div>
                     Hệ thống phòng khám
                 </div>
