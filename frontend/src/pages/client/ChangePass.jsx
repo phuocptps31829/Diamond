67628d2c7c6e613067e507344b1ef9e150ec2612
Nguyen Tan Vinh <vutrongphung1212@gmail.com>
2024-07-26 03:27:08 +0700

    vink up code nhen

---
+++ frontend/src/pages/client/ChangePass.jsx
@@ -0,0 +1,8 @@
+import ChangePassComponent from "../../components/client/Account/changepass/changepass";
+
+export default function ChangePass() {
+    return (
+        <ChangePassComponent/>
+    )
+    ;
+}
