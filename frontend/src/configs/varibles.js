5a6794cbab6ceed55be4080b53faa3816da6b76e
tiendat2204 <121950007+tiendat2204@users.noreply.github.com>
2024-07-30 16:50:09 +0700

    dat

---
+++ frontend/src/configs/varibles.js
@@ -0,0 +1,5 @@
+const API_URL = "https://x0dvd1wq-3500.asse.devtunnels.ms";
+//service
+const API_URL_GET_ALL_SERVICES = `${API_URL}/services`;
+
+export { API_URL_GET_ALL_SERVICES };
---
+++ frontend/src/services/servicesApi.js
@@ -0,0 +1,13 @@
+
+import { API_URL_GET_ALL_SERVICES } from "@/configs/varibles";
+import axios from "axios";
+export const getAllServices = async () => {
+
+    try {
+      const res = await axios.get(`${API_URL_GET_ALL_SERVICES}`);
+      console.log(res.data.data);
+      return res.data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
