7c76e2bb832d4b83435e020607fc65b83a78bbe0
phuongvtps <150603161+phuongvtps@users.noreply.github.com>
2024-08-08 13:20:20 +0700

    Delete frontend directory

--- frontend/src/pages/client/Accuracy.jsx
+++
@@ -1,6 +0,0 @@
-import AccuracyComponent from "../../components/client/Account/accuracy/Accuracy";
-import useScrollToTop from "@/hooks/useScrollToTop";
-export default function Accurancy() {
-  useScrollToTop();
-  return <AccuracyComponent />;
-}
--- frontend/src/pages/client/Register.jsx
+++
@@ -1,9 +0,0 @@
-import RegisterComponent from "../../components/client/Account/register/Register";
-import useScrollToTop from "@/hooks/useScrollToTop";
-export default function Register() {
-    useScrollToTop();
-    return (
-        <RegisterComponent />
-    )
-        ;
-}
