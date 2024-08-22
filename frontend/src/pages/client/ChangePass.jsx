f49d26074010f8370bcd0484494d677daec7767d
sw-n2-9h <chinhnguyennn24@gmail.com>
2024-08-22 15:59:08 +0700

    before vinh

--- frontend/src/pages/client/ChangePass.jsx
+++
@@ -1,9 +0,0 @@
-import ChangePassComponent from "../../components/client/Account/changePass/ChangePass";
-import useScrollToTop from "@/hooks/useScrollToTop";
-export default function ChangePass() {
-    useScrollToTop();
-    return (
-        <ChangePassComponent />
-    )
-        ;
-}
--- frontend/src/pages/client/ForgetPassWord.jsx
+++
@@ -1,9 +0,0 @@
-import ForgetComponent from "../../components/client/Account/forgetPassword/Forget";
-import useScrollToTop from "@/hooks/useScrollToTop";
-export default function ForgetPassword() {
-    useScrollToTop();
-    return (
-        <ForgetComponent />
-    )
-        ;
-}
